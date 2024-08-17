231248f3a9567aedd69620c5e1a08988ae313a3a
pruthasavani44 <prutha.micrasol@gmail.com>
2024-08-17 09:50:58 +0530

    Add title

--- src/componants/Title.jsx
+++ src/componants/Title.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Title() {
-  return <div>Title</div>;
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>
+        <h1>NPM Project</h1>
+        <p>Cretare React js. Project Using NPM</p>
+      </div>
+    </>
+  );
 }
